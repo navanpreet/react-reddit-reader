db692d3092a84dc62ce6d356e26e4974254c9867
Navan <navanpreet09@gmail.com>
2017-12-05 14:12:21 -0800

    Update reducers.js

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -6,15 +6,16 @@ export default (state=defaultState, action) => {
   switch(action.type) {
     case "TOGGLE_SUBREDDIT":
       const newSubReddit = action.subReddit;
-      const index = state.subReddits.indexOf(newSubReddit)
+      let clone = Object.assign({}, state.subReddits);
+      const index = clone.indexOf(newSubReddit);
       if(index === -1){
-        state.subReddits.unshift(newSubReddit);
+        clone.unshift(newSubReddit);
       } else {
-        state.subReddits.splice(index, 1);
+        clone.splice(index, 1);
       }
       return {
         ...state,
-        subReddits: [...state.subReddits]
+        subReddits: [...clone]
       };
     default:
       return state;
